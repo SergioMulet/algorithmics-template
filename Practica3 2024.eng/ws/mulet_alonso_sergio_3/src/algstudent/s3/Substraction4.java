6b8627984285aed506af00bd4e4c6d1873845d1d
Sergio Mulet Alonso <UO296503@uniovi.es>
2024-02-29 19:48:58 +0100

    practice3 class

---
+++ Practica3 2024.eng/ws/mulet_alonso_sergio_3/src/algstudent/s3/Substraction4.java
@@ -0,0 +1,33 @@
+package algstudent.s3;
+
+public class Substraction4 {
+	static long cont;
+
+	public static void rec4(int n) {
+
+		if (n <= 0) {
+			cont++;
+		} else {
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j< n; j++) {
+					cont++;
+				}
+			}
+			rec4(n-1);
+		}
+			
+	}
+
+	public static void main(String arg[]) {
+		long t1, t2, cont = 0;
+		for (int n = 100; n <= 1000000; n *= 2) {
+			t1 = System.currentTimeMillis();
+
+			rec4(n);
+
+			t2 = System.currentTimeMillis();
+
+			System.out.println("n=" + n + "**TIME=" + (t2 - t1) + "**cont=" + cont);
+		} // for
+	} // main
+}
