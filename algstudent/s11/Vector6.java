420ccc58fb2e5dc3014884ca9910187d60f039eb
Sergio Mulet Alonso <UO296503@uniovi.es>
2025-02-15 21:00:05 +0100

    session1.1 finished

---
+++ algstudent/s11/Vector6.java
@@ -0,0 +1,30 @@
+package s11;
+
+public class Vector6 {
+
+	static int[]a;
+	static int[]b;
+	private static int k = 2;
+	
+	public static void main(String arg []) {
+		int repetitions = Integer.parseInt(arg[0]);
+		long t1,t2;
+		int sum = 0;
+		
+		for (int n=10000; n<=Integer.MAX_VALUE; n*=k){ //n is increased *5   
+			  a = new int[n];
+			  b = new int[n];
+			  Vector1.fillIn(a);
+			  Vector1.fillIn(b);
+			  
+			  t1 = System.currentTimeMillis();
+			  //We have to repeat the whole process to be measured
+			  for (int repetition=1; repetition<=repetitions; repetition++){    	
+			     Vector1.matches1(a, b);
+			  }
+			  t2 = System.currentTimeMillis();
+			  System.out.printf("SIZE=%d TIME=%d milliseconds SUM=%d NTIMES=%d\n", n, t2-t1, sum, repetitions);	
+		}//for 
+		
+	}//main
+}
